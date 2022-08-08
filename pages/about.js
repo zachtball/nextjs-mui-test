020b58423bdecca303c732104ef3793c7422432d
Zachary Ball <zacharyball@Zacharys-MacBook-Pro.local>
2022-08-08 16:53:37 -0700

    upgrade next to canary

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from 'mui-test-lib';
+import { Button } from 'mui-test-lib';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
